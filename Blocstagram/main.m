5927cf51782479813f39a258a6e04c40ad68505f
Rohan Khara <khararohan@gmail.com>
2014-12-05 15:43:22 +0530

    Initial Commit

---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Rohan on 05/12/14.
+//  Copyright (c) 2014 Rohan Khara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

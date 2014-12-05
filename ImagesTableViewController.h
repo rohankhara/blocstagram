8793d8ed99e660d5c941e46d0813e3ec56ebf778
Rohan Khara <khararohan@gmail.com>
2014-12-05 16:48:00 +0530

    Intermediate commit

--- ImagesTableViewController.h
+++ ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Rohan on 05/12/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
